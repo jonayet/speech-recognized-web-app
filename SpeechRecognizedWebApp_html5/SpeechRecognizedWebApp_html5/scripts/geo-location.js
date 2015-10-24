516a301794d7f8cf9b8dcc888955b8deb3ca561f
Jonayet Hossain <jonayet.bu@gmail.com>
2015-10-25 01:15:34 +0600

    updated

---
+++ SpeechRecognizedWebApp_html5/SpeechRecognizedWebApp_html5/scripts/geo-location.js
@@ -0,0 +1,45 @@
+﻿(function (navigator) {
+    "use_strict";
+    if (!navigator.geolocation) {
+        console.log("Geolocation is not supported");
+    }
+
+    window.gps = {};
+
+    function rad(x) {
+        return x * Math.PI / 180;
+    }
+
+    // Distance in kilometers between two points using the Haversine algo.
+    function getDistance(p1, p2) {
+        var r = 6371;
+        var dLat = rad(p2.latitude - p1.latitude);
+        var dLong = rad(p2.longitude - p1.longitude);
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.latitude)) * Math.sin(dLong / 2) * Math.sin(dLong / 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = r * c;
+        return Math.round(d * 1000) / 1000;
+    }
+
+    function position(coords) {
+        this.coords = coords;
+        return {
+            coords: coords,
+            getDistance: function (targetCoords) {
+                return getDistance(coords, targetCoords);
+            }
+        }
+    }
+
+    function getLocation(callback) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            if (callback) {
+                callback(new position(pos.coords));
+            }
+        });
+    }
+
+    window.gps.getLocation = getLocation;
+    window.gps.getDistance = getDistance;
+})(window.navigator);
--- SpeechRecognizedWebApp_html5/SpeechRecognizedWebApp_html5/scripts/main.js
+++ SpeechRecognizedWebApp_html5/SpeechRecognizedWebApp_html5/scripts/main.js
@@ -1,34 +1,87 @@
-﻿(function ($, speechApi) {
+﻿(function ($, gps, speechApi) {
     "use_strict";
+
+    var targetCoordinates = { latitude: 22.811044, longitude: 90.361507 };
+    var targetAddress = "Dhaka";
+    var areaRadius = 2.5; // Km
 
     if (!speechApi) {
         console.log("speechApi not supported");
         return;
     }
 
-    function askForSOmething() {
-        speechApi.speak("please say something...", function() {
+    function confirm(success, aborted) {
+        speechApi.speak("Confirm by Yes or No. Or say Cancel to skip.");
+        speechApi.listen(function(confirmResp) {
+            switch (confirmResp.text) {
+            case "yes":
+                if (success)
+                    success(true);
+                break;
+            case "no":
+                if (success)
+                    success(false);
+                break;
+                case "cancel":
+                    speechApi.speak("canceled.");
+                if (aborted)
+                    aborted(true);
+                break;
+            default:
+                speechApi.speak("unknown response.");
+                confirm(success, aborted);
+            }
+        });
+    }
+
+    function askForInput(text, success, aborted) {
+        speechApi.speak(text, function () {
             speechApi.listen(function (result) {
                 if (!result.error) {
-                    speechApi.speak("did you said?." + result.text + ". please confirm.");
-                    speechApi.listen(function(result) {
-                        if (result.text === "yes") {
-                            speechApi.speak("text recognized succed");
-                            return;
-                        } else {
-                            askForSOmething();
-                        }
+                    speechApi.speak("did you said?" + result.text, function () {
+                        confirm(function (yes) {
+                            if (yes && success)
+                                success(result.text);
+                            if (!yes)
+                                askForInput(text, success, aborted);
+                        }, function() {
+                            if (aborted)
+                                aborted();
+                        });
                     });
-
                 } else {
-                    speechApi.speak("some error occured.");
+                    speechApi.speak("error occured. please try again.");
                 }
             });
         });
     }
 
-    $("#btn_saySomething").click(function () {
-        askForSOmething();
+    $("#btn_start").click(function () {
+        $('#btn_start').prop('disabled', true);
+        gps.getLocation(function (position) {
+            var distance = position.getDistance(targetCoordinates);
+            if (distance < areaRadius) {
+                speechApi.speak("are you around " + targetAddress, function () {
+                    confirm(function(yes) {
+                        if (yes) {
+                            askForInput("please give some input.", function (text) {
+                                speechApi.speak("you have entered: " + text  + ":" + ".thank you.", function() {
+                                    $('#btn_start').prop('disabled', false);
+                                });
+                            }, function () {
+                                $('#btn_start').prop('disabled', false);
+                            });
+                        } else {
+                            $('#btn_start').prop('disabled', false);
+                        }
+                    }, function() {
+                        $('#btn_start').prop('disabled', false);
+                    });
+                });
+            } else {
+                $('#btn_start').prop('disabled', false);
+            }
+        });
     });
 
-})(window.$, window.speechApi);
+})(window.$, window.gps, window.speechApi);
