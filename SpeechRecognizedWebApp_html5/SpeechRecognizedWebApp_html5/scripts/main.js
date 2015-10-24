da0410349e5cb1dd948ffb0ef80248c9b505fbc7
Jonayet Hossain <jonayet.bu@gmail.com>
2015-10-24 14:03:32 +0600

    updated

---
+++ SpeechRecognizedWebApp_html5/SpeechRecognizedWebApp_html5/scripts/main.js
@@ -0,0 +1,17 @@
+﻿(function ($, speak) {
+    "use_strict";
+
+    if (!speak) {
+        console.log("I can't speak!");
+        return;
+    }
+
+    function sayHello() {
+        speak("Hello!");
+    }
+
+    $("#btn_sayHello").click(function() {
+        sayHello();
+    });
+
+})(window.$, window.speak);
---
+++ SpeechRecognizedWebApp_html5/SpeechRecognizedWebApp_html5/scripts/speech-synthesis.js
@@ -0,0 +1,29 @@
+﻿(function (speechSynthesis, speechSynthesisUtterance) {
+    "use_strict";
+
+    if (!speechSynthesis) {
+        console.log("speechSynthesis is not supported");
+        return;
+    }
+
+    function speak(text, callback) {
+        var u = new speechSynthesisUtterance();
+        u.text = text;
+        u.lang = 'en-US';
+
+        u.onend = function () {
+            if (callback) {
+                callback();
+            }
+        };
+
+        u.onerror = function (e) {
+            if (callback) {
+                callback(e);
+            }
+        };
+        speechSynthesis.speak(u);
+    }
+
+    window.speak = speak;
+})(window.speechSynthesis, window.SpeechSynthesisUtterance);
